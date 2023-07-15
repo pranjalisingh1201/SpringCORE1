d2d6917c4ff816e4c82c9562e70c62309be42435
pranjalisingh1201 <pranjalisingh1201@gmail.com>
2023-07-15 16:32:29 +0530

    ApplicationContext Implementations.

--- src/main/java/com/springcore/spring1/App.java
+++ src/main/java/com/springcore/spring1/App.java
@@ -2,6 +2,8 @@ package com.springcore.spring1;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.FileSystemResource;
 
 /**
@@ -20,7 +22,16 @@ public class App
     	 * We are type casting the bean in Alien object.
     	 * Calling Code method.
     	 * */
-        BeanFactory factory = new XmlBeanFactory(new FileSystemResource("Spring.xml"));
+        //BeanFactory factory = new XmlBeanFactory(new FileSystemResource("Spring.xml"));
+    	/*
+    	 * BeanFactory and XmlBeanFactory are deprecated and new updated methods are not present in these interfaces and classes.
+    	 * Hence we use Application Context interface to implement getBean method.
+    	 * ClassPathXMLApplicationContext and all the other ApplicationContext files are extended by Application Context itself.
+    	 * 
+    	 *  We have to move Spring.xml to class path directory which is main/java since it is looking in class path directory itself.
+    	 */
+    	ApplicationContext factory = new ClassPathXmlApplicationContext("Spring.xml");
+    	
     	Alien obj = (Alien)factory.getBean("alien");
     	obj.code();
     }
